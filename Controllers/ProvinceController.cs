eb168da85eb45cf817ee22ed25dbcae8cd84e549
haile <hailv2.21it@vku.udn.vn>
2022-12-17 16:50:32 +0700

    api for province, district, ward list

---
+++ Controllers/ProvinceController.cs
@@ -0,0 +1,56 @@
+using System.Data;
+using System.Data.Common;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Residence.DTOs;
+using Residence.Entities;
+using Residence.Enums;
+using RestSharp;
+
+namespace Residence.Controllers;
+
+public class ProvinceController : BaseApiController
+{
+    private readonly ILogger<ProvinceController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public ProvinceController(ILogger<ProvinceController> logger, IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        var client = new RestClient("https://provinces.open-api.vn/api/p/");
+        var request = new RestRequest("", Method.Get);
+        var response = await client.ExecuteAsync(request);
+        var result = JsonSerializer.Deserialize<dynamic>(response.Content!);
+        return Ok(result);
+    }
+
+    [HttpGet("districts/{code}")]
+    public async Task<ActionResult> GetDistricts(string code)
+    {
+        var client = new RestClient($"https://provinces.open-api.vn/api/p/{code}?depth=2");
+        var request = new RestRequest("", Method.Get);
+        var response = await client.ExecuteAsync(request);
+        var result = JsonSerializer.Deserialize<dynamic>(response.Content!);
+        return Ok(result);
+    }
+
+    [HttpGet("districts/{code}/wards")]
+    public async Task<ActionResult> GetWards(string code)
+    {
+        var client = new RestClient($"https://provinces.open-api.vn/api/d/{code}?depth=2");
+        var request = new RestRequest("", Method.Get);
+        var response = await client.ExecuteAsync(request);
+        var result = JsonSerializer.Deserialize<dynamic>(response.Content!);
+        return Ok(result);
+    }
+
+}
