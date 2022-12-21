60f1b0b86c7ecacbd766ce2d7125f8eb4e3110ec
hai_le <hailv2.21it@vku.udn.vn>
2022-12-21 21:30:31 +0700

    final

---
+++ DTOs/ResidenceInformationAdminRequestDto.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Residence.DTOs;
+
+public class ResidenceInformationAdminRequestDto
+{
+    [Required]
+    public int? IdUsers { get; set; }
+    [Required]
+    public string? Fullname { get; set; }
+    [Required]
+    public string? Gender { get; set; }
+    [Required]
+    public DateTime? Birthday { get; set; }
+    [Required]
+    public string? City { get; set; }
+    [Required]
+    public string? District { get; set; }
+    [Required]
+    public string? Ward { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Phone { get; set; }
+    [Required]
+    public string? Address { get; set; }
+    [Required]
+    public int IDToKhai { get; set; }
+    public string? TrangThai { get; set; }
+}
